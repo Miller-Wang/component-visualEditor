938a701ab7178a23c44257cb16bc9af4c3f1c326
mengliang.wang <millerw@wicresoft.com>
2021-01-23 10:46:14 +0800

    布局

--- demo-visual-editor/src/App.vue
+++ demo-visual-editor/src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <h1>这是页面内容</h1>
+    <visual-editor />
   </div>
-  <router-view/>
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { VisualEditor } from "../src/packages/visual-editor";
+
+export default defineComponent({
+  name: "App",
+  components: { VisualEditor },
+});
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html,
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
